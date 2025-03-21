7174f2aa7914aefd7a94cc93b8a9955c693e563d
novanurhamdani <nova.nurhamdani@gmail.com>
2025-03-21 14:23:03 +0700

    feat: setup nativewind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex h-full justify-center items-center bg-blue-500">
+      <Text>Hello World</Text>
+    </View>
+  );
+}
